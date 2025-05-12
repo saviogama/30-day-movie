8ddbf555386d66f8f3dcc2b3644cb29f114f40c7
Savio Gama <savio.gama09@gmail.com>
2025-05-12 20:53:51 -0300

    chore(services): add headers to api requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'x-api-key': import.meta.env.VITE_INTERNAL_API_KEY,
+  },
 });
