6098b5a59067f69517e4512a5cf09f18225beb26
Savio Gama <savio.gama09@gmail.com>
2025-05-13 00:12:58 -0300

    fix(home): fix select movies section check

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -17,6 +17,7 @@ export default function Home() {
     if (
       shouldRenderSelectMoviesSection &&
       !isLoadingRecommendations &&
+      !hasErrorOnRecommendationsFetch &&
       !hasRecommendations
     ) {
       return (
